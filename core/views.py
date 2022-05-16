a46a5377084190fc1728d0828e2d1000762b74ec
Smyja <akposlive59@gmail.com>
2022-05-16 09:41:11 +0100

    setup

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from news.models import Stories
+from .serializers import StoriesSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
